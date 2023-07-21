bf7b76d90a0e0f7cce508f3eb52a475bfbffc585
Rafael La Buonora <rlabuonora@opp.gub.uy>
2023-07-21 17:16:10 +0000

    More inputs

--- ui.R
+++ ui.R
@@ -24,6 +24,16 @@ body <- dashboardBody(
                               choices = unique(programs_geolocated$subject), 
                               selected = unique(programs_geolocated$subject)[c(1, 12)],
                               multiple = TRUE)),
+           box(width=4, status="warning",
+               h4("Requirements"),
+               sliderInput("ielts", "IELTS",
+                           min = 0, 
+                           max = 9, 
+                           value = c(0, 9)),
+               sliderInput("toefl", "TOEFL",
+                           min = 0, 
+                           max = 120, 
+                           value = c(0, 120))),
            box(width = 2, status = "warning",
                checkboxGroupInput("study_level", "Study Level",
                                   choices = unique(programs_geolocated$study_level),
@@ -38,15 +48,7 @@ body <- dashboardBody(
                            min = constants$fee[1], 
                            max = constants$fee[2], 
                            value = round(c(constants$fee[1],
-                                     constants$fee[2]))),
-               sliderInput("ielts", "IELTS",
-                           min = 0, 
-                           max = 9, 
-                           value = c(0, 9)),
-               sliderInput("toefl", "TOEFL",
-                           min = 0, 
-                           max = 120, 
-                           value = c(0, 120)))
+                                     constants$fee[2]))))
     )
   )
 )
