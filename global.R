ee301e86720b73325d903658b5dff32a7551f010
Rafael La Buonora <rlabuonora@opp.gub.uy>
2023-07-21 12:27:32 +0000

    Layout 1

---
+++ global.R
@@ -0,0 +1,43 @@
+library(fresh)
+library(dplyr)
+
+# Create the theme
+mytheme <- create_theme(
+  adminlte_color(
+    light_blue = "#434C5E"
+  ),
+  adminlte_sidebar(
+    width = "400px",
+    dark_bg = "#D8DEE9",
+    dark_hover_bg = "#81A1C1",
+    dark_color = "#2E3440"
+  ),
+  adminlte_global(
+    content_bg = "#FFF",
+    box_bg = "#D8DEE9", 
+    info_box_bg = "#D8DEE9"
+  )
+)
+
+# load data
+
+programs_geolocated <- readRDS("~/apps/university/data/programs_geolocated.rds") %>% 
+  select(university, longitude, latitude, requirements, study_level, location, program_title, subject,
+         study_mode, course_intensity, duration, fee_gbp, toefl, ielts, bachelor_gpa,
+         cambridge_cae_advanced, pte_academic, a_levels, international_baccalaureate)
+
+the_ranking_data <- readRDS("~/apps/university/data/the_ranking_data.rds") %>% 
+  filter(university != "ESCP Business School - Paris")
+
+
+# Helper function to filter programs within map bounds
+# de bounds
+filter_bounds <- function(data, bounds) {
+  latRng <- range(bounds$north, bounds$south)
+  lngRng <- range(bounds$east, bounds$west)
+  
+  subset(data,
+         latitude >= latRng[1] & latitude <= latRng[2] &
+           longitude >= lngRng[1] & longitude <= lngRng[2])
+}
+
---
+++ server.R
@@ -0,0 +1,57 @@
+library(shinydashboard)
+library(leaflet)
+library(htmltools)
+library(fresh)
+library(DT)
+
+function(input, output, session) {
+  
+  
+  inBounds <- reactive({
+    
+    
+    if (is.null(input$mapa_bounds))
+      return(the_ranking_data[FALSE,])
+    
+    bounds <- input$mapa_bounds
+    list(programs=filter_bounds(programs_geolocated, bounds),
+         locations=filter_bounds(the_ranking_data, bounds))
+
+  })
+  
+  output$programs_tbl <- renderDataTable({
+    datatable(inBounds()$programs %>% 
+                select(program_title, study_level, study_mode, course_intensity,
+                       duration, fee_gbp, university))
+  })
+  
+
+
+  output$mapa <- renderLeaflet({
+    
+
+    
+    leaflet(the_ranking_data) %>%
+      addProviderTiles("CartoDB.Positron") %>% 
+      addCircleMarkers()
+      # addCircleMarkers(color="goldenrod",
+      #                  group="CAIF",
+      #                  #layerId = ~ruee,
+      #                  data=caif,
+      #                  label = ~htmlEscape(nombre),
+      #                  clusterOptions = markerClusterOptions()) %>%
+      # addCircleMarkers(color="red",
+      #                  group="ANEP",
+      #                  #layerId = ~ruee,
+      #                  data=escuelas,
+      #                  label = ~htmlEscape(nombre),
+      #                  clusterOptions = markerClusterOptions()) %>%
+      # leaflet::addLegend(position = "bottomright",
+      #                    values = ~inst, # data frame column for legend
+      #                    opacity = .7, # alpha of the legend
+      #                    pal = palPwr, # palette declared earlier
+      #                    title = "") %>%
+      # leaflet::addLayersControl(overlayGroups = c("CAIF", "ANEP"),
+      #                           options = layersControlOptions(collapsed = FALSE)) %>%
+  })
+}
